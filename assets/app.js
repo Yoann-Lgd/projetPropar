34b771a6f90ab18284981b13d3d4c11385bbb4ed
tomtom7157 <tom.men@hotmail.com>
2022-01-02 21:26:13 +0100

    Webpack part2 et boostrap 5.1.3

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,6 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+// or get all of the named exports for further usage
+import * as bootstrap from 'bootstrap';
