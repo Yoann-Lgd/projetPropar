791407792a5dc53f9672d604c674a20f591f5ca7
tomtom7157 <tom.men@hotmail.com>
2022-01-03 01:28:11 +0100

    ajout file-loader@^6.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,19 @@ Encore
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
 
+    .copyFiles({
+        from: './assets/images',
+        
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+        
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+        
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
+    
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
