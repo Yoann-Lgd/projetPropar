3fc58f08eb8d700050e3750c60835c25de4a3023
tomtom7157 <tom.men@hotmail.com>
2022-01-03 14:56:24 +0100

    migration de la bdd

---
+++ migrations/Version20220103135548.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220103135548 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE operation CHANGE statut_operation_id statut_operation_id INT NOT NULL, CHANGE utilisateur_id utilisateur_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE operation CHANGE statut_operation_id statut_operation_id INT DEFAULT NULL, CHANGE utilisateur_id utilisateur_id INT NOT NULL');
+    }
+}
