de57555069344be0b03c05bbd4a5b3894fe6a50d
leromanichel <ugopenet@mac.com>
2022-01-10 15:05:16 +0100

    compteur dans encours operation

--- src/Controller/OperationController.php
+++ src/Controller/OperationController.php
@@ -46,7 +46,7 @@ class OperationController extends AbstractController
                     $operation->setStatutOperation($statutOperation);
                     $entityManager->persist($operation);
                     $entityManager->flush();
-                    $this->addFlash('success', 'Votre opération a bien été crée et affecté à '.$operation->getUtilisateur().$maxOperation);
+                    $this->addFlash('success', 'Votre opération a bien été crée et affecté à '.$operation->getUtilisateur());
                 } else {
                     $this->addFlash('error', 'Votre opération n\'a pas été crée car l\'utilisateur  '.$operation->getUtilisateur().' a atteint son cota');
                 }
